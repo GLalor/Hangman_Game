1b81b46fcbd6a517e3bb3a3e4eac9c1ddc182cd4
Graham <grahamlalor@gmail.com>
2019-03-10 11:34:53 +0000

    push 1

---
+++ hangmanScript.py
@@ -0,0 +1,141 @@
+import random
+import os
+clear = lambda: os.system('cls')
+clear()
+
+words = ["case","list","wage","deer","sour"]
+global lettersGuessed
+lettersGuessed = []
+global correctGues
+correctGues = []
+
+def main():
+    gameLogic()
+
+def gameLogic():
+    guesedWrong = 0
+    word = words[1] # words[random.randint(0,3)]
+    printHangman(guesedWrong, correctGues, word)
+    while guesedWrong <= 6:
+        #printCorrectLetters()
+        letter = userInput(guesedWrong, word)
+        if letter in word and letter != '':
+            correctGues.append(letter)
+            printHangman(guesedWrong, correctGues, word)
+            print(correctGues)
+        else: 
+            guesedWrong = guesedWrong + 1
+            printHangman(guesedWrong, correctGues, word)
+    printFinalWord(word)
+
+
+def userInput(guesedWrong, word):
+    letter = ''
+    print()
+    while letter not in lettersGuessed:
+        print ("\033[A                             \033[A")
+        letter = input("Guess a letter:")
+        if letter in lettersGuessed:
+            print("Letter already guessed!")
+        else:
+            lettersGuessed.append(letter)
+            return letter
+    return ''
+
+
+def printHangman(guesedWrong, correct, word):
+    clear()
+    print("Lets play hangman!!!")
+    print("---------------------")
+    printRules()
+    for i in range(0,6):
+        if i == 0:
+            print("")
+        elif i == 1:
+            print("#######")
+        else:
+            if guesedWrong > 0:
+                printWrongAnswers(guesedWrong)
+                break
+            else:
+                print("#")
+    if len(correct) > 0:
+        printCorrectLetters(correctGues,word)
+    else:
+     printSpaces(word)
+
+def sortCorrectLetters(correct,wordPicked):
+    
+
+def printSpaces(wordPicked):
+    print()
+    for i in range(0, len(wordPicked)):
+        print("-", end="")
+    print("\n")
+
+def printCorrectLetters(correct,wordPicked):
+    print()
+    for i in range(0, len(correct)):
+        position = wordPicked.index(correct[i])
+        for y in range(0, len(wordPicked)):
+            if y != position:
+                print(" ", end="")
+            elif y == position:
+                print(correct[i], end="")
+    printSpaces(wordPicked)
+
+def printFinalWord(wordPicked):
+    print("The word was: ",wordPicked)
+
+
+
+def printWrongAnswers(numGuesses):
+    if numGuesses == 1:
+        print("#   |")
+        printTags(3)
+    elif numGuesses == 2:
+        print("#   |")
+        print("#   0")
+        printTags(2)
+    elif numGuesses == 3:
+        print("#   |")
+        print("#   0")
+        print("#   |")
+        printTags(1)
+    elif numGuesses == 4:
+        print("#   |")
+        print("#   0")
+        print("#  /|")
+        printTags(1)
+    elif numGuesses == 5:
+        print("#   |")
+        print("#   0")
+        print("#  /|\\")
+        printTags(1)
+    elif numGuesses == 6:
+        print("#   |")
+        print("#   0")
+        print("#  /|\\")
+        print("#  /")
+    elif numGuesses == 7:
+        printFullBody()
+
+
+def printTags(numTags):
+    if numTags > 0:
+        for i in range(0, numTags):
+            print("#")
+
+def printFullBody():
+    print("#   |")
+    print("#   0")
+    print("#  /|\\")
+    print("#  /\\")
+    print("\n  R.I.P")
+
+def printRules():
+    print("Rules:")
+    print("----  7 Guesses  ----")
+
+if __name__ == '__main__':
+   main()
